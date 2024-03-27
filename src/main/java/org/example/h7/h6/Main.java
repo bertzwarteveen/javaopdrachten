a5744bc11428e7076603d029f3522c04315cedcc
bertzwarteveen <e.zwarteveen@belastingdienst.nl>
2024-03-27 09:42:44 +0100

    hfd 6 opdracht

--- src/main/java/org/example/h7/h6/Main.java
+++ src/main/java/org/example/h7/h6/Main.java
@@ -5,16 +5,17 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Methods methods = new Methods();
-   //     System.out.println(Arrays.toString(methods.doubleArray(new long[] {4, 3, 32, 5})));
-    //    System.out.println(Arrays.toString(methods.multiplyItems(new long[] {4, 3, 32, 5}, 65)));
+        //     System.out.println(Arrays.toString(methods.doubleArray(new long[] {4, 3, 32, 5})));
+        //    System.out.println(Arrays.toString(methods.multiplyItems(new long[] {4, 3, 32, 5}, 65)));
         System.out.println(Arrays.toString(methods.run(66)));
 
         long[] row = new long[4];
         row[2] = 3;
         long[] copy = row;
         copy[2]++;
+        System.out.println(copy[2]);
         System.out.println(row[2]); // same reference
-     //   System.out.println(copy[2]);
-   //     System.out.println(row[4]); // Out of bounds max [3]
+        //   System.out.println(copy[2]);
+        //     System.out.println(row[4]); // Out of bounds max [3]
     }
 }
